e7c215879b667eeb555fd2440f754dec5e98762a
wfeng98-1664973 <32472023+wfeng98-1664973@users.noreply.github.com>
2019-11-19 02:08:36 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ library(shiny)
 library(plotly)
 library(dplyr)
 
-suicide_rate <- read.csv("suicide_rate.csv")
+suicide_rate <- read.csv("./data/suicide_rate.csv")
 ages <- unique(suicide_rate$age)
 # Use a fluid Bootstrap layout
 fluidPage(    
