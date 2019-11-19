92d94347b31f302586a322deabaeede0cce110df
Freezer <freezeryang@gmail.com>
2019-11-19 02:23:04 -0800

    in progress

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ suicide_rate <- function(input, output) {
   output$phonePlot <- renderPlot({
     
     # Render a plot
-    suicide_rate <- read.csv("suicide_rate.csv")
+    suicide_rate <- read.csv("./data/suicide_rate.csv")
     df <-suicide_rate[suicide_rate$sex==input$sex & suicide_rate$age==input$age,]
     ggplot(data=df, aes(x=population)) + geom_histogram()
   })
